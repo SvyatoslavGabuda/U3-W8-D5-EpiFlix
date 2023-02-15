55533b4dcb0c751cd08f539f32d7c66480952052
SvyatoslavGabuda <sgabuda@gmail.com>
2023-02-15 14:02:06 +0100

    1

--- src/components/MyMain.jsx
+++ src/components/MyMain.jsx
@@ -1,69 +1,77 @@
-import { Component } from "react";
-import MyTitle from "./MyTitle";
+import { useState } from "react";
 import { Container, Form, FloatingLabel, Button, Row, Col } from "react-bootstrap";
+import MyTitle from "./MyTitle";
 import FilmRow from "./FilmRow,";
 import NavInMain from "./NavInMain";
 
-class MyMain extends Component {
-  state = {
-    newSearch: "",
-    searching: false,
-  };
-  startSearch = () => {
-    if (this.state.newSearch !== "") {
-      this.setState({ searching: true });
+const MyMain = () => {
+  // state = {
+  //   newSearch: "",
+  //   searching: false,
+  // };
+  const [newSearch, setNewSearch] = useState("");
+  const [searching, setSearching] = useState(false);
+  const startSearch = () => {
+    if (newSearch !== "") {
+      // this.setState({ searching: true });
+      setSearching(true);
     } else {
       console.log("nope");
       alert("inserisci qualcosa nel campo di ricerca");
     }
   };
-  render() {
-    return (
-      <>
-        <Container fluid className="px-4">
-          <NavInMain />
 
-          {/* Mytitle è semplicemente un titolo mentre la FilmRow riceve una props e crea una row contenenti i film trovati */}
-          <MyTitle title="Trending Now" />
-          <FilmRow search="wick" />
+  return (
+    <>
+      <Container fluid className="px-4">
+        <NavInMain />
 
-          {/* Blocco che si occupa della ricerca della ricerca */}
-          <MyTitle title="Search:" />
-          <Row className="align-items-start">
-            <Col xs="8">
-              <FloatingLabel
-                controlId="floatingInput"
-                label="Tipe the Name of the film you want to see!"
-                className="mb-3"
-              >
-                <Form.Control
-                  type="search"
-                  placeholder="jaws"
-                  value={this.state.newSearch}
-                  onChange={(e) => this.setState({ searching: false, newSearch: e.target.value })}
-                />
-              </FloatingLabel>
-            </Col>
-            <Col className="h-100">
-              <Button
-                className="btn btn-secondary btn-sm  rounded-0 p-3"
-                style={{ backgroundColor: "#221f1f" }}
-                onClick={this.startSearch}
-              >
-                <i className="fa fa-search icons"></i> Start Searching
-              </Button>
-            </Col>
-          </Row>
-          {this.state.searching && <FilmRow search={this.state.newSearch} />}
-          {/* fine blocco ricerca */}
+        {/* Mytitle è semplicemente un titolo mentre la FilmRow riceve una props e crea una row contenenti i film trovati */}
+        <MyTitle title="Trending Now" />
+        <FilmRow search="wick" />
 
-          <MyTitle title="Watch It Again" />
-          <FilmRow search="jaws" />
-          <MyTitle title="New Releases" />
-          <FilmRow search="star" />
-        </Container>
-      </>
-    );
-  }
-}
+        {/* Blocco che si occupa della ricerca della ricerca */}
+        <MyTitle title="Search:" />
+        <Row className="align-items-start">
+          <Col xs="8">
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Tipe the Name of the film you want to see!"
+              className="mb-3"
+            >
+              <Form.Control
+                type="search"
+                placeholder="jaws"
+                value={newSearch}
+                onChange={
+                  (e) => {
+                    setSearching(false);
+                    setNewSearch(e.target.value);
+                  }
+                  // this.setState({ searching: false, newSearch: e.target.value })
+                }
+              />
+            </FloatingLabel>
+          </Col>
+          <Col className="h-100">
+            <Button
+              className="btn btn-secondary btn-sm  rounded-0 p-3"
+              style={{ backgroundColor: "#221f1f" }}
+              onClick={startSearch}
+            >
+              <i className="fa fa-search icons"></i> Start Searching
+            </Button>
+          </Col>
+        </Row>
+        {searching && <FilmRow search={newSearch} />}
+        {/* fine blocco ricerca */}
+
+        <MyTitle title="Watch It Again" />
+        <FilmRow search="jaws" />
+        <MyTitle title="New Releases" />
+        <FilmRow search="star" />
+      </Container>
+    </>
+  );
+};
 export default MyMain;
