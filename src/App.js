22c94d2a4f327d5e67172b3eb77c06a4d152d55c
SvyatoslavGabuda <sgabuda@gmail.com>
2023-02-15 14:06:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.scss";
 import MyFooter from "./components/MyFooter";
 import MyMain from "./components/MyMain";
 import MyNav from "./components/MyNav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
